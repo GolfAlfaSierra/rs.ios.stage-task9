d83a0eed13ce3af4824437c433910fde7d4c1263
Artyom Sog <tomas.tomson@Mail.ru>
2021-08-03 15:45:57 +0300

    additional

--- RSSchool_T9/RSSchool_T9/Controllers/Items/Views/ItemsView.swift
+++ RSSchool_T9/RSSchool_T9/Controllers/Items/Views/ItemsView.swift
@@ -3,12 +3,27 @@
 // Project: RSSchool_T9
 // 
 // Author: John Kent
-// On: 01.08.2021
+// On: 03.08.2021
 // 
 // Copyright © 2021 RSSchool. All rights reserved.
 
 import UIKit
 
-class ItemsView: UIView {
+class ItemsView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        
+        setupSelf()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSelf()
+    }
+    
+    func setupSelf() {
+        self.contentInset = UIEdgeInsets(top: 30, left: 20, bottom: 50, right: 20)
+    }
     
 }
